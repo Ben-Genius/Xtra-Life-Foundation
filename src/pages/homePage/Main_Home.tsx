ea99cf054eb2d92ae5631391166ca7ef6a6eff8e
Ben-Genius <bernard.wiafe@afrilogicsolutions.com>
2023-09-17 16:36:49 +0000

    Index homePage

--- src/pages/homePage/Main_Home.tsx
+++ src/pages/homePage/Main_Home.tsx
@@ -1,20 +1,20 @@
-import { Header } from "../../components/Header"
 import Hero from "./Hero"
 import Welcome from "./Welcome"
 import CardSwiper from "./CardSwiper"
 import Partners from "./Partners"
 import Community from "./Community"
 import Project from "./Project"
+import Volunteer from "./Volunteer"
 const Main_Home = () => {
   return (
     <div>
-      <Header/>
       <Hero/>
       <Welcome/>
       <CardSwiper/>
       <Partners/>
       <Community/>
       <Project/>
+      <Volunteer/>
 </div>
   )
 }
