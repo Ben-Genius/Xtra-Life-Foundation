ebc5b5efee51428cd965677ef20847f66aa67907
Ben-Genius <bernard.wiafe@afrilogicsolutions.com>
2023-09-17 16:38:35 +0000

    re-route to support lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import {Routes, Route} from 'react-router-dom'
 import Main_Home from './pages/homePage/Main_Home'
 import { Main_Donate } from './pages/donate/Main_Donate'
-
+import { Header } from './components/Header'
+import Footer from './components/Footer'
+import { xtraFooter } from './data/data'
+import OurStory from './pages/aboutUs/OurStory/ourStory'
+import Team from './pages/aboutUs/Team/team'
+import Advisory from './pages/aboutUs/AdvisoryBoard/advisory'
 const App = () => {
   return (
     <div>
+          <Header/>
+
    <Routes>
     <Route path="/" element={<Main_Home/>}/>
     <Route path="donate" element={<Main_Donate/>}/>
+    <Route path="about" element={<OurStory/>}/>
+    <Route path="team" element={<Team/>}/>
+    <Route path="advisory-board" element={<Advisory/>}/>
+
    </Routes>
+   <Footer xtraFooter={xtraFooter}/>
+
     </div>
   )
 
