1bf188093314066e8f45a9ddefda688c0f88367e
Ben-Genius <bernard.wiafe@afrilogicsolutions.com>
2023-10-04 02:42:55 +0000

    Main Advisory compponent

--- src/pages/aboutUs/AdvisoryBoard/Advisory.tsx
+++ src/pages/aboutUs/AdvisoryBoard/Advisory.tsx
@@ -1,7 +1,47 @@
+import AdvisoryCard from "./AdvisoryCard";
+import { advisory } from "../../../data/data";
 
 const Advisory = () => {
+
+
   return (
-    <div>
+    <div className="overflow-hidden">
+       <div
+        className="bg-cover give overlay relative text-white max-w-full h-[17rem] sm:h-screen  bg-center bg-no-repeat bg-imgSm sm:bg-imgMd lg:bg-imgLg"
+
+      >
+        <div className="p-3 md:p-5 w-full mb-3 flex flex-col items-center h-full justify-center relative">
+          
+          <h1 className="font-bold sm:max-w-full text-4xl sm:6xl  text-center font-sans">FOR THE FUTURE GHANA ADVISORY BOARD </h1>
+
+        </div>
+</div>
+          <section className="max-w-full sm:max-w-[94%] mx-auto">
+          <div className=" p-10 bg-blue-500 mt-16">
+              <h1 className="font-bold text-white text-xl sm:text-2xl md:text-4xl"> XTF ADVISORY BOARD </h1>
+              <p className="text-md text-white mt-9">
+                Duis deserunt sunt ex esse consequat aute qui.Irure pariatur enim adipisicing tempor.
+                Aute dolor sint aute aliquip <b className="text-green-500">reprehenderit proident consectetur ullamco aliquip </b>laborum officia commodo consequat.
+                Aute consectetur deserunt consectetur tempor nostrud magna.Esse duis anim non excepteur dolor nostrud reprehenderit ipsum nostrud exercitation ea pariatur deserunt incididunt.Fugiat incididunt sunt tempor ex sint sint cupidatat nulla reprehenderit aliqua sit qui.
+              </p>
+          </div>
+
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4 w-full gap-4 mt-12">
+
+          {advisory.map((member, index) => (
+               <AdvisoryCard 
+                   key={index} 
+                   image={member.image} 
+                   name={member.name} 
+                   position={member.position} 
+               />
+           ))}
+
+          </div>
+          </section>
+
+      
     </div>
   )
 }
