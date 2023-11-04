99ce0829d64360a2bb753ddd65682221141ed819
Ben-Genius <bernard.wiafe@afrilogicsolutions.com>
2023-11-04 22:56:11 +0000

    Use lazy Loading to bundle application

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
 import {Routes, Route} from 'react-router-dom'
 import Main_Home from './pages/homePage/Main_Home'
-import { Main_Donate } from './pages/donate/Main_Donate'
+// import { Main_Donate } from './pages/donate/Main_Donate'
 import { Header } from './components/Header'
 import Footer from './components/Footer'
 import { xtraFooter } from './data/data'
-import ProjectOne from './pages/projects/ProjectOne'
-import Advisory from './pages/aboutUs/AdvisoryBoard/Advisory'
-import OurStory from './pages/aboutUs/OurStory/OurStory'
-import Team from './pages/aboutUs/Team/Team'
+// import ProjectOne from './pages/projects/ProjectOne'
+// import Team from './pages/aboutUs/Team/Team'
 import Events from './pages/event/Events'
-import ContactUs from './pages/contactUs/ContactUs'
+import React, {Suspense} from 'react'
+
+const OurStory = React.lazy(() => import ("./pages/aboutUs/OurStory/OurStory"))
+const Team = React.lazy(() => import ("./pages/aboutUs/Team/Team"))
+const AdvisoryBoard = React.lazy(() => import ("./pages/aboutUs/AdvisoryBoard/Advisory"))
+const Project = React.lazy(() => import ("./pages/projects/ProjectOne"))
+const ContactUs = React.lazy(() => import ("./pages/contactUs/ContactUs"))
+
 
 const App = () => {
   return (
     <div>
-          <Header/>
+          <Header/> 
 
    <Routes>
     <Route path="/" element={<Main_Home/>}/>
-    <Route path="donate" element={<Main_Donate/>}/>
-    <Route path="about" element={<OurStory/>}/>
-    <Route path="team" element={<Team/>}/>
-    <Route path="advisory-board" element={<Advisory/>}/>
-    <Route path="project" element={<ProjectOne/>}/>
+    {/* <Route path="donate" element={<Main_Donate/>}/> */}
+    <Route path="about" element={<Suspense fallback =" 'Loading" > <OurStory/></Suspense>}/>
+    <Route path="team" element={<Suspense fallback =" 'Loading" ><Team/></Suspense>}/>
+    <Route path="advisory-board" element={<Suspense fallback =" 'Loading" > <AdvisoryBoard/> </Suspense>}/>
     <Route path ="events" element={<Events/>}/>
-    <Route path="contact-us" element={<ContactUs/>} />
+    <Route path="contact-us" element={<Suspense fallback =" 'Loading" > <ContactUs/> </Suspense>} />
+    <Route path="project" element={<Suspense fallback =" 'Loading" > <Project/> </Suspense>} />
+
    </Routes>
    <Footer xtraFooter={xtraFooter}/>
 
