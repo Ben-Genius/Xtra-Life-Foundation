40329ad1cd9edd08e0f92056a6cd9437470c8101
Ben-Genius <bernard.wiafe@afrilogicsolutions.com>
2023-11-05 21:48:48 +0000

    Updated contactUs page

--- src/pages/contactUs/ContactUs.tsx
+++ src/pages/contactUs/ContactUs.tsx
@@ -1,64 +1,78 @@
-import { motion } from 'framer-motion';
-import {
-  Text,
-  Title,
-  SimpleGrid,
-  TextInput,
-  Textarea,
-  Button,
-  Group,
-  ActionIcon,
-} from '@mantine/core';
-import {RiTwitterLine, RiYoutubeFill,RiInstagramFill} from 'react-icons/ri';
-import { ContactIconsList } from './ContactIcons';
+import { TextInput, Textarea, SimpleGrid, Group, Title, Button } from '@mantine/core';
 
-const social = [RiTwitterLine, RiYoutubeFill,RiInstagramFill];
-
-export default function ContactUs() {
-  const icons = social.map((Icon, index) => (
-    <ActionIcon key={index} size={28} variant="transparent">
-      <Icon size="1.4rem" stroke={1.5} />
-    </ActionIcon>
-  ));
+const ContactUs = () => {
+  // const form = useForm({
+  //   initialValues: {
+  //     name: '',
+  //     email: '',
+  //     subject: '',
+  //     message: '',
+  //   },
+  //   validate: {
+  //     name: (value) => value.trim().length < 2,
+  //     email: (value) => !/^\S+@\S+$/.test(value),
+  //     subject: (value) => value.trim().length === 0,
+  //   },
+  // });
 
   return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="p-8">
-      <SimpleGrid spacing={50} className="grid-cols-1 md:grid-cols-2 gap-12">
-        <motion.div initial={{ y: -20 }} animate={{ y: 0 }} transition={{ delay: 0.1 }}>
-          <Title className="text-2xl font-semibold mb-4">Contact us</Title>
-          <Text className="text-sm mb-8">
-            Leave your email and we will get back to you within 24 hours
-          </Text>
+    // <form onSubmit={form.onSubmit(() => {})}>
 
-          <ContactIconsList />
+    <form  className='w-[95%] mx-auto'>
+      <Title
+        order={2}
+        size="h1"
+        style={{ fontFamily: 'Greycliff CF, var(--mantine-font-family)' }}
+        fw={900}
+        ta="center"
+      >
+        Get in touch
+      </Title>
 
-          <Group className="mt-10 space-x-4">{icons}</Group>
-        </motion.div>
-        <motion.div initial={{ y: 20 }} animate={{ y: 0 }} transition={{ delay: 0.2 }}>
-          <TextInput
-            label="Email"
-            placeholder="your@email.com"
-            required
-            className="mb-4"
-          />
-          <TextInput
-            label="Name"
-            placeholder="John Doe"
-            className="mb-4"
-          />
-          <Textarea
-            required
-            label="Your message"
-            placeholder="I want to order your goods"
-            minRows={4}
-            className="mb-4"
-          />
+      <SimpleGrid className="grid-cols-1 md:grid-cols-2 gap-12" mt="xl">
+        <TextInput
+          label="Name"
+          placeholder="Your name"
+          name="name"
+          variant="filled"
+          // {...form.getInputProps('name')}
+        />
+        <TextInput
+          label="Email"
+          placeholder="Your email"
+          name="email"
+          variant="filled"
+          // {...form.getInputProps('email')}
+        />
+      </SimpleGrid>
 
-            <Group  mt="md">
-              <Button className='bg-blue-500 hover:bg-blue-600' >Send message</Button>
-            </Group>
-          </motion.div>
-        </SimpleGrid>
-      </motion.div>
-    );
-  }
+      <TextInput
+        label="Subject"
+        placeholder="Subject"
+        mt="md"
+        name="subject"
+        variant="filled"
+        // {...form.getInputProps('subject')}
+      />
+      <Textarea
+        mt="md"
+        label="Message"
+        placeholder="Your message"
+        maxRows={10}
+        minRows={5}
+        autosize
+        name="message"
+        variant="filled"
+        // {...form.getInputProps('message')}
+      />
+
+      <Group  mt="xl" className='justify-center '>
+        <Button type="submit" size="md">
+          Send message
+        </Button>
+      </Group>
+    </form>
+  );
+}
+
+export default ContactUs
